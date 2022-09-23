0db48af2f0bfe2ddb2dea7be72c6a359db94b4fb
AnikDasPartha58 <Anikdaspartha58@gmail.com>
2022-09-23 22:48:58 +0600

    make dark icon button

---
+++ scripts/theme.js
@@ -0,0 +1,26 @@
+const toggle = document.getElementById("sun_icon");
+const body = document.querySelector("body");
+const first = document.querySelector(".first");
+const middle = document.querySelector(".middle");
+const last = document.querySelector(".last");
+const about = document.querySelector(".about");
+const moon = document.querySelector(".bi-brightness-high");
+
+toggle.addEventListener("click", function () {
+  this.classList.toggle("bi-moon");
+  if (this.classList.toggle("bi-brightness-high")) {
+    body.style.background = "white";
+    first.style.color = "black";
+    middle.style.color = "black";
+    last.style.color = "black";
+    about.style.color = "black";
+    moon.style.color = "black";
+  } else {
+    body.style.background = "black";
+    first.style.color = "white";
+    middle.style.color = "white";
+    last.style.color = "white";
+    about.style.color = "white";
+    moon.style.color = "white";
+  }
+});
