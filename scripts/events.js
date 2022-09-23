99cce21a50e28478d3965134d2f941c3b16bae6f
AnikDasPartha58 <Anikdaspartha58@gmail.com>
2022-09-24 00:37:14 +0600

    ready for deployment

--- scripts/events.js
+++ scripts/events.js
@@ -2,37 +2,37 @@ $(document).ready(function () {
   $("#calendar").evoCalendar({
     todayHighlight: "true",
     calendarEvents: [
+      //  september
       {
-        id: "event1", // Event's ID (required)
-        name: "New Year", // Event name (required)
-        date: "January/1/2022", // Event date (required)
-        type: "holiday",
-        description: "the first day of the year",
+        id: "sept1", // Event's ID (required)
+        name: "American Chess Day", // Event name (required)
+        date: "September/1/2022", // Event date (required)
+        type: "event",
         color: "red", // Event type (required)
         everyYear: true, // Same event every year (optional)
       },
       {
-        name: "Vacation Leave",
-        badge: "02/13 - 02/15", // Event badge (optional)
-        date: ["February/13/2022", "February/15/2022"], // Date range
-        description: "Vacation leave for 3 days.", // Event description (optional)
+        id: "sept1", // Event's ID (required)
+        name: "Building and Code Staff Appreciation Day", // Event name (required)
+        date: "September/1/2022", // Event date (required)
         type: "event",
-        color: "#63d867", // Event custom color (optional)
-      },
-      {
-        id: "event1", // Event's ID (required)
-        name: "New Year", // Event name (required)
-        date: "January/1/2022", // Event date (required)
-        type: "holiday",
-        color: "blue", // Event type (required)
+        color: "red", // Event type (required)
         everyYear: true, // Same event every year (optional)
       },
       {
-        id: "event1", // Event's ID (required)
-        name: "New Year", // Event name (required)
-        date: "January/1/2022", // Event date (required)
-        type: "holiday",
-        color: "yellow", // Event type (required)
+        id: "sept1", // Event's ID (required)
+        name: "Cherry Popover Day", // Event name (required)
+        date: "September/1/2022", // Event date (required)
+        type: "event",
+        color: "red", // Event type (required)
+        everyYear: true, // Same event every year (optional)
+      },
+      {
+        id: "sept1", // Event's ID (required)
+        name: "Chicken bo Day", // Event name (required)
+        date: "September/1/2022", // Event date (required)
+        type: "event",
+        color: "red", // Event type (required)
         everyYear: true, // Same event every year (optional)
       },
     ],
