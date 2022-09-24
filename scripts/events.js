36d6b98084e9771f2fb1cce7d00a8ac973b1c583
AnikDasPartha58 <Anikdaspartha58@gmail.com>
2022-09-25 02:13:53 +0600

    adding events

--- scripts/events.js
+++ scripts/events.js
@@ -2,108 +2,275 @@ $(document).ready(function () {
   $("#calendar").evoCalendar({
     todayHighlight: "true",
     calendarEvents: [
-      //  september
+      //  september start
+      //-- sept1--//
       {
-        id: "sept1", // Event's ID (required)
-        name: "American Chess Day", // Event name (required)
-        date: "September/1/2022", // Event date (required)
+        id: "sept1",
+        name: "American Chess Day",
+        date: "September/1/2022",
         type: "event",
-        description:
-          "A game of strategy and skill, chess is played the world over by young and old alike. To play, two opponents face off across a checkers-style board, and take turns using their pieces of kings, queens, rooks, knights, bishops, and pawns to move along the 64-square board. Game times vary — great players can knock out their opponents very quickly or can spend hours strategizing their every move. To win, a player must use his pieces to capture those of the other player, with the ultimate goal of taking over the other player’s king. Checkmate! Often called “The Game of Kings,” you can play chess the good old-fashioned way with another player in person, or on the computer with opponents from all over the world.",
-        everyYear: true, // Same event every year (optional)
+        everyYear: true,
+        color: "#004EFF",
       },
       {
-        id: "sept1", // Event's ID (required)
-        name: "Building and Code Staff Appreciation Day", // Event name (required)
-        date: "September/1/2022", // Event date (required)
+        id: "sept1",
+        name: "Building and Code Staff Appreciation Day",
+        date: "September/1/2022",
         type: "event",
-        description:
-          "Building and Code Staff Appreciation Day is celebrated every year on September 1. For all the troubles that they undoubtedly face every day, the folks in charge of maintaining building and code standards don’t get as much gratitude. Think about it; checking a building and approving it as secure according to code is not easy. Anything overlooked can endanger people living in an unsafe building. This is why it is important that we show our appreciation for them",
+        everyYear: true,
+        color: "#004EFF",
+      },
+      {
+        id: "sept1",
+        name: "Cherry Popover Day",
+        date: "September/1/2022",
+        type: "event",
+        everyYear: true,
+        color: "#004EFF",
+      },
+      {
+        id: "sept1",
+        name: "Chicken Boy Day",
+        date: "September/1/2022",
+        type: "event",
+        everyYear: true,
+        color: "#004EFF",
+      },
+      {
+        id: "sept1",
+        name: "Emma M Nutt Day",
+        date: "September/1/2022",
+        type: "event",
+        everyYear: true,
+        color: "#004EFF",
+      },
+      {
+        id: "sept1",
+        name: "Ginger Cat Appreciation Day",
+        date: "September/1/2022",
+        type: "event",
+        everyYear: true,
+        color: "#004EFF",
+      },
+      {
+        id: "sept1",
+        name: "International Cabernet Day",
+        date: "September/1/2022",
+        type: "event",
+        everyYear: true,
+        color: "#004EFF",
+      },
+      {
+        id: "sept1",
+        name: "National Acne Positivity Day(US)",
+        date: "September/1/2022",
+        type: "event",
+        everyYear: true,
+        color: "#004EFF",
+      },
+      {
+        id: "sept1",
+        name: "	National Child Identity Theft Awareness Day(US)",
+        date: "September/1/2022",
+        type: "event",
+        everyYear: true,
+        color: "#004EFF",
+      },
+      {
+        id: "sept1",
+        name: "	National Little Black Dress Day(US)",
+        date: "September/1/2022",
+        type: "event",
+        everyYear: true,
+        color: "#004EFF",
+      },
+      {
+        id: "sept1",
+        name: "	National Monterey County Fair Day(US)",
+        date: "September/1/2022",
+        type: "event",
+        everyYear: true,
+        color: "#004EFF",
+      },
+      {
+        id: "sept1",
+        name: "	National No Rhyme Nor Reason Day(US)",
+        date: "September/1/2022",
+        type: "event",
+        everyYear: true,
+        color: "#004EFF",
+      },
+      {
+        id: "sept1",
+        name: "National Tofu Day(UK)",
+        date: "September/1/2022",
+        type: "event",
+        everyYear: true,
+        color: "#004EFF",
+      },
+      {
+        id: "sept1",
+        name: "Pink Cadillac Day",
+        date: "September/1/2022",
+        type: "event",
+        everyYear: true,
+        color: "#004EFF",
+      },
+      {
+        id: "sept1",
+        name: "Slovakia Constitution Day",
+        date: "September/1/2022",
+        type: "event",
+        everyYear: true,
+        color: "#004EFF",
+      },
+      {
+        id: "sept1",
+        name: "Uzbekistan Independence Day",
+        date: "September/1/2022",
+        type: "event",
+        everyYear: true,
+        color: "#004EFF",
+      },
+      {
+        id: "sept1",
+        name: "Wattle Day",
+        date: "September/1/2022",
+        type: "event",
+        everyYear: true,
+        color: "#004EFF",
+      },
+      {
+        id: "sept1",
+        name: "World Letter Writing Day",
+        date: "September/1/2022",
+        type: "event",
+        everyYear: true,
+        color: "#004EFF",
+      },
 
-        everyYear: true, // Same event every year (optional)
+      //-- sept2--//
+      {
+        id: "sept2",
+        name: "Bison-ten Yell Day",
+        date: "September/2/2022",
+        type: "event",
+        everyYear: true,
+        color: "#004EFF",
       },
       {
-        id: "sept1", // Event's ID (required)
-        name: "Cherry Popover Day", // Event name (required)
-        date: "September/1/2022", // Event date (required)
+        id: "sept2",
+        name: "Bring Your Manners To Work Day",
+        date: "September/2/2022",
         type: "event",
-        description:
-          "Cherry Popover Day is celebrated on September 1 every year. This day honors the delicious treat, the cherry popover, which is enjoyed and loved by many. But what exactly is a cherry popover? A cherry popover is a light and airy roll that is made from egg batter, topped with cherries, and baked in a pan. Popovers are said to be English-inspired. Settlers from Maine who founded Portland, Oregon created the popover by giving the English Yorkshire pudding an American spin. They can be consumed both as a sweet treat or as a proper breakfast or brunch meal.",
-
-        everyYear: true, // Same event every year (optional)
+        everyYear: true,
+        color: "#004EFF",
       },
       {
-        id: "sept1", // Event's ID (required)
-        name: "Ginger Cat Appreciation Day", // Event name (required)
-        date: "September/1/2022", // Event date (required)
+        id: "sept2",
+        name: "Calendar Adjustment Day",
+        date: "September/2/2022",
         type: "event",
-        description:
-          "Ginger Cat Appreciation Day brings a little love to ginger cats. Sometimes called marmalade cats or tiger cats, ginger cats are orange tabby cats. Tabby cats have striped, marbled, ticked, or spotted markings on their coats, and almost always have a marking in the shape of an M on their forehead. All orange cats are tabbies, but not all tabbies are orange. There are no solid orange or ginger cats. Ginger cats come in shades of orange, gold, and red. Most ginger cats are male, and both domestic or wild cats can be ginger cats.While all ginger cats are honored today, one particular ginger cat, Doobert, is recognized today for his role in helping change animal transport. Chris Roy created a software platform that helps coordinate animal rescue relay transport, which is used by rescue organizations and volunteers, and named it after Doobert, his late ginger cat. Doobert is by no means the only ginger cat to have ever garnered a bit of attention. Milo of The Adventures of Milo and Otis fame, Garfield, Winston Churchill's cat, Jock, Jones from Alien, and Orangey, who starred in Breakfast at Tiffany's, among other films, all have had their time in the spotlight. But whether famous or not, all ginger cats are celebrated today!",
-        everyYear: true, // Same event every year (optional)
+        everyYear: true,
+        color: "#004EFF",
       },
       {
-        id: "sept1", // Event's ID (required)
-        name: "National Acne Positivity Day", // Event name (required)
-        date: "September/1/2022", // Event date (required)
+        id: "sept2",
+        name: "Ceuta Day",
+        date: "September/2/2022",
         type: "event",
-        description:
-          "National Acne Positivity Day is observed next on Friday, September 1st, 2023. It has been observed annually on September 1st since 2020.",
-        everyYear: true, // Same event every year (optional)
+        everyYear: true,
+        color: "#004EFF",
       },
       {
-        id: "sept1", // Event's ID (required)
-        name: "National Burnt Ends Day", // Event name (required)
-        date: "September/1/2022", // Event date (required)
+        id: "sept2",
+        name: "College Colors Day",
+        date: "September/2/2022",
         type: "event",
-        description:
-          "National Burnt Ends Day is observed next on Friday, September 1st, 2023. It has been observed annually on September 1st since 2021.",
-        everyYear: true, // Same event every year (optional)
+        everyYear: true,
+        color: "#004EFF",
       },
       {
-        id: "sept1", // Event's ID (required)
-        name: "National No Rhyme or Reason Day", // Event name (required)
-        date: "September/1/2022", // Event date (required)
+        id: "sept2",
+        name: "	National Blueberry Popsicle Day(US)",
+        date: "September/2/2022",
         type: "event",
-        description:
-          "You snooze, you will be deprived. Fake it till you get there. These phrases just don't have any rhyme to them. That's no problem today, though, because National No Rhyme or Reason Day is for creating rhymes that don't actually rhyme. English words that don't rhyme with any other words are known as refractory rhymes, and using them makes National No Rhyme or Reason Day quite easy to celebrate. While there may be some obscure words that rhyme with purple, toilet, and orange, there aren't many, and when words like these are used while forming rhymes today, the phrases likely won't have much rhyme or reason to them.",
-        everyYear: true, // Same event every year (optional)
+        everyYear: true,
+        color: "#004EFF",
       },
       {
-        id: "sept1", // Event's ID (required)
-        name: "National Tofu Day (UK)", // Event name (required)
-        date: "September/1/2022", // Event date (required)
+        id: "sept2",
+        name: "National Chianti Day(US)",
+        date: "September/2/2022",
         type: "event",
-        description:
-          "National Tofu Day (UK) is observed next on Friday, September 1st, 2023. It has been observed annually on September 1st since 2018.",
-        everyYear: true, // Same event every year (optional)
+        everyYear: true,
+        color: "#004EFF",
       },
       {
-        id: "sept1", // Event's ID (required)
-        name: "Pink Cadillac Day", // Event name (required)
-        date: "September/1/2022", // Event date (required)
+        id: "sept2",
+        name: "National Day of Vietnam",
+        date: "September/2/2022",
         type: "event",
-        description:
-          "This day is dedicated to pink Cadillacs, which have an iconic place in American culture, most notably because of Elvis Presley. Elvis mentioned a pink Cadillac in his first charting single, Baby Let's Play House, which was released in April 1955, and bought his first pink Cadillac around the same time. It was a 1954 Fleetwood Series 60, and it was destroyed in a roadside fire in June of the same year. His second Cadillac was a 1955 Fleetwood Series 60, which was originally blue with a black roof. He had a neighbor paint it pink for him, and gave it to his mother Gladys as a gift. As Gladys didn't have a driver's licence, Elvis still did most of the driving of it. Eventually Elvis had the top changed from black to white too. Elvis also bought himself a white with pink roofed 1961 Cadillac Coupe de Ville upon returning home from his service in Germany, and let his friend and soon-to-be tour manager, Joe Esposito, borrow the 1955 car. The 1955 is his most famous Cadillac, and it now is on display in Graceland.Although Elvis 1955 car was a four-door sedan, the romanticized version of the pink Cadillac in popular culture is seen as being the 1959 Cadillac Series 62 convertible. Many miniature replicas of this car have been made, and it has been featured in music videos. Pink Cadillacs have also permeated the public consciousness by being given to Mary Kay Cosmetics associates, after they reach $100,000 in sales in a year. Mary Kay Ash, the founder of Mary Kay, originally had a Cadillac made for herself based off a blush color, and sales directors liked it so much they bought their own. In 1969, Mary Kay Ash gave her type five sellers pink blush colored Cadillacs. Today cars are still being given out, although associates have an option to pick more cars besides just the pink Cadillac.",
-        everyYear: true, // Same event every year (optional)
+        everyYear: true,
+        color: "#004EFF",
       },
       {
-        id: "sept1", // Event's ID (required)
-        name: "World Letter Writing Day", // Event name (required)
-        date: "September/1/2022", // Event date (required)
+        id: "sept2",
+        name: "National Food Bank Day(US)",
+        date: "September/2/2022",
         type: "event",
-        description:
-          "World Letter Writing Day is observed next on Friday, September 1st, 2023. It has been observed annually on September 1st since 2014.",
-        everyYear: true, // Same event every year (optional)
+        everyYear: true,
+        color: "#004EFF",
       },
       {
-        id: "sept1", // Event's ID (required)
-        name: "Toy Tips Executive Toy Test Day", // Event name (required)
-        date: "September/1/2022", // Event date (required)
+        id: "sept2",
+        name: "National Lazy Mom’s Day(US & Other)",
+        date: "September/2/2022",
         type: "event",
-        description:
-          "Toy Tips Executive Toy Test Day is observed next on Friday, September 1st, 2023. It is observed annually on September 1st.          ",
-        everyYear: true, // Same event every year (optional)
+        everyYear: true,
+        color: "#004EFF",
       },
+      {
+        id: "sept2",
+        name: "National Live Fearless Day(US)",
+        date: "September/2/2022",
+        type: "event",
+        everyYear: true,
+        color: "#004EFF",
+      },
+      {
+        id: "sept2",
+        name: "Pierce Your Ears Day",
+        date: "September/2/2022",
+        type: "event",
+        everyYear: true,
+        color: "#004EFF",
+      },
+      {
+        id: "sept2",
+        name: "	Spalding Baseball Day",
+        date: "September/2/2022",
+        type: "event",
+        everyYear: true,
+        color: "#004EFF",
+      },
+      {
+        id: "sept2",
+        name: "World Coconut Day",
+        date: "September/2/2022",
+        type: "event",
+        everyYear: true,
+        color: "#004EFF",
+      },
+      {
+        id: "sept2",
+        name: "	VJ Day(US)",
+        date: "September/2/2022",
+        type: "event",
+        everyYear: true,
+        color: "#004EFF",
+      },
+      // september exit
     ],
   });
 });
